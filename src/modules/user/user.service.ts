b56ad4eb9b9d7a6e91ce2f46c9969d843f4c5da7
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 00:15:58 -0500

    fear: populate findByEmail

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from './schemas/user.schema';
-import mongoose, { Model } from "mongoose";
-
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { User, UserDocument } from "./schemas/user.schema";
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel:Model<UserDocument>) { }
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return "This action adds a new user";
   }
 
   findAll() {
@@ -22,7 +21,7 @@ export class UserService {
   }
 
   async findByEmail(email: string) {
-    const foundUser = await this.userModel.findOne({ email: email  });
+    const foundUser = (await this.userModel.findOne({ email: email })).populate("person");
     if (!foundUser) throw Error(`User ${email} not found`);
     return foundUser;
   }
