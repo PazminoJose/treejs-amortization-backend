aabe8b4d4024edba441905ae9ccd2a3d29213087
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 00:16:11 -0500

    feat: export user service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -14,6 +14,7 @@ import { UserService } from "./user.service";
     ])
   ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
