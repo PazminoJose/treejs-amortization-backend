0937b3a75665cd5de759a6572ad4ed09c52d3475
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 00:16:52 -0500

    feat: add _id in user schema

--- src/modules/person/schemas/person.schema.ts
+++ src/modules/person/schemas/person.schema.ts
@@ -1,10 +1,12 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import mongoose, { Document, HydratedDocument } from "mongoose";
 
 export type PersonDocument = HydratedDocument<Person>;
 
 @Schema({ versionKey: false })
-export class Person {
+export class Person extends Document {
+  _id: mongoose.Types.ObjectId;
+
   @Prop({ required: true })
   dni: string;
 
@@ -19,7 +21,6 @@ export class Person {
 
   @Prop({ required: true })
   lastName: string;
-
 }
 
 export const PersonSchema = SchemaFactory.createForClass(Person);
