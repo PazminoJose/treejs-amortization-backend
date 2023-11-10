4ebdbd2a125d3134d4db14507bf7341b4aecdc77
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 11:29:02 -0500

    feat: add create with session methods

--- src/modules/person/person.service.ts
+++ src/modules/person/person.service.ts
@@ -1,15 +1,13 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { CreatePersonDto } from './dto/create-person.dto';
-import { UpdatePersonDto } from './dto/update-person.dto';
-import { Person, PersonDocument } from './schemas/person.schema';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from "mongoose";
-import { HttpErrorException } from 'src/commons/exceptions/http-error.exception';
-
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import mongoose, { Model } from "mongoose";
+import { HttpErrorException } from "src/commons/exceptions/http-error.exception";
+import { CreatePersonDto } from "./dto/create-person.dto";
+import { Person, PersonDocument } from "./schemas/person.schema";
 
 @Injectable()
 export class PersonService {
-  constructor(@InjectModel(Person.name) private readonly personModel: Model<PersonDocument>) { }
+  constructor(@InjectModel(Person.name) private readonly personModel: Model<PersonDocument>) {}
 
   async create(createPersonDto: CreatePersonDto) {
     const foundPerson = await this.personModel.findOne({ dni: createPersonDto.dni });
@@ -18,7 +16,14 @@ export class PersonService {
     return createdPerson;
   }
 
+  async createWithSession(createPersonDto: CreatePersonDto, session: mongoose.mongo.ClientSession) {
+    const foundPerson = await this.personModel.findOne({ dni: createPersonDto.dni });
+    if (foundPerson) throw new HttpErrorException("Error al encontrar al usuario", HttpStatus.BAD_REQUEST);
+    const createdPerson = await this.personModel.create([createPersonDto], { session });
+    return createdPerson[0];
+  }
+
   async findAll() {
-    return await this.personModel.find()
+    return await this.personModel.find();
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,19 +1,32 @@
+import { HttpErrorException } from "@commons";
 import { HttpStatus, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { hash } from "bcrypt";
+import mongoose, { Model } from "mongoose";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
 import { User, UserDocument } from "./schemas/user.schema";
-import { HttpErrorException } from "@commons";
 
 @Injectable()
 export class UserService {
+  private readonly SALT_ROUNDS = 10;
+
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
   async create(createUserDto: CreateUserDto) {
     const foundUser = this.userModel.findOne({ email: createUserDto.email });
-    if(foundUser) throw new HttpErrorException("Error al encontrar el usuario", HttpStatus.BAD_REQUEST);
+    if (foundUser) throw new HttpErrorException("El usuario ya existe", HttpStatus.BAD_REQUEST);
     const createdUser = await this.userModel.create(createUserDto);
+    if (!createdUser) throw new HttpErrorException("Error al crear el usuario", HttpStatus.BAD_REQUEST);
     return createdUser;
+  }
+
+  async createWithSession(createUserDto: CreateUserDto, session: mongoose.mongo.ClientSession) {
+    const foundUser = this.userModel.findOne({ email: createUserDto.email });
+    if (foundUser) throw new HttpErrorException("El usuario ya existe", HttpStatus.BAD_REQUEST);
+    createUserDto.password = await hash(createUserDto.password, this.SALT_ROUNDS);
+    const createdUser = await this.userModel.create([createUserDto], { session });
+    if (createdUser.length === 0)
+      throw new HttpErrorException("Error al crear el usuario", HttpStatus.BAD_REQUEST);
+    return createdUser[0];
   }
 
   async findAll() {
@@ -22,8 +35,6 @@ export class UserService {
 
   async findByEmail(email: string) {
     const foundUser = (await this.userModel.findOne({ email: email })).populate("person");
-    if (!foundUser) throw new HttpErrorException("Error al encontrar el usuario", HttpStatus.BAD_REQUEST)
     return foundUser;
   }
-
 }
