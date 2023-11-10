05466388795f6062bdb7ad9d59f02bb002b03de5
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 11:28:51 -0500

    chore: formatting

--- src/modules/user-rol/user-rol.service.ts
+++ src/modules/user-rol/user-rol.service.ts
@@ -1,23 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserRolDto } from './dto/create-user-rol.dto';
-import { UpdateUserRolDto } from './dto/update-user-rol.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { UserRol, UserRolDocument } from './schemas/user-rol.schema';
-import { Model } from 'mongoose';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { CreateUserRolDto } from "./dto/create-user-rol.dto";
+import { UserRol, UserRolDocument } from "./schemas/user-rol.schema";
 
 @Injectable()
 export class UserRolService {
-  constructor(@InjectModel(UserRol.name) private userRolModel: Model<UserRolDocument> ) { }
+  constructor(@InjectModel(UserRol.name) private userRolModel: Model<UserRolDocument>) {}
   async create(createUserRolDto: CreateUserRolDto) {
     const createdUserRol = await this.userRolModel.create(createUserRolDto);
     return createdUserRol;
   }
 
-
   async findOne(userId: string) {
-    const foundUserRol = (await this.userRolModel.findOne({user: userId})).populate("rol");
+    const foundUserRol = await this.userRolModel.find({ user: userId }).populate("rol");
     return foundUserRol;
   }
-  
-
 }
