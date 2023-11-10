3765f53700985294c68ccf5ea816b390d6ee15af
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 10:56:14 -0500

    feat: Add filters and modules to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { APP_FILTER } from "@nestjs/core";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { FolderConstants } from "./commons/constants";
+import { AllExceptionsFilter } from "./commons/filters";
 import { AuthModule } from "./modules/auth/auth.module";
+import { CompanyCreditTypeModule } from "./modules/company-credit-type/company-credit-type.module";
 import { CompanyModule } from "./modules/company/company.module";
+import { CreditTypeModule } from "./modules/credit-type/credit-type.module";
 import { PersonModule } from "./modules/person/person.module";
 import { RolModule } from "./modules/rol/rol.module";
+import { UserCompanyModule } from "./modules/user-company/user-company.module";
 import { UserRolModule } from "./modules/user-rol/user-rol.module";
 import { UserModule } from "./modules/user/user.module";
-import { UserCompanyModule } from './modules/user-company/user-company.module';
-import { CreditTypeModule } from './modules/credit-type/credit-type.module';
-import { CompanyCreditTypeModule } from './modules/company-credit-type/company-credit-type.module';
 
 @Module({
   imports: [
@@ -37,6 +39,11 @@ import { CompanyCreditTypeModule } from './modules/company-credit-type/company-c
     CompanyCreditTypeModule
   ],
   controllers: [],
-  providers: []
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter
+    }
+  ]
 })
 export class AppModule {}
